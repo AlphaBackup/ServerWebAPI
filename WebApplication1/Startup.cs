8283a28c6ad15e3d6db92af9a731d605286857cc
Bohdan Duda <duda.bohdan23@gmail.com>
2022-04-02 17:33:48 +0200

    pridan balicek NewtonsoftJson

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -26,7 +26,10 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
