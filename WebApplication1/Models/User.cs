136c644dd3038eda9cc763492340fd4dc9858cf0
Bohdan Duda <duda.bohdan23@gmail.com>
2022-04-03 15:14:07 +0200

    Odebrani nepouzitych importu

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
-    [Table("clients")]
+    [Table("user")]
     public class User
     {
         [Column("id")]
